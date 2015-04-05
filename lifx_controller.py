d131bde5bf31c0b716449760e91394c09a068afb
Max Bainrot <mbainrot@gmail.com>
2015-04-05 19:32:13 +1000

    Should have fixed all occurances of incorrect multi line concats (fixes #6)

--- lifx_controller.py
+++ lifx_controller.py
@@ -20,7 +20,7 @@ def _lifx_json_get(url):
     buffer = BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, lifx_config.lifx_apiserver + url)
-    c.setopt(c.HTTPHEADER, ['Authorization: Bearer ' +
+    c.setopt(c.HTTPHEADER, ['Authorization: Bearer ' + \
                             lifx_config.lifx_apikey])
     c.setopt(c.WRITEDATA, buffer)
     c.perform()
@@ -35,7 +35,7 @@ def _lifx_json_put(url, data):
     buffer = BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, lifx_config.lifx_apiserver + url)
-    c.setopt(c.HTTPHEADER, ['Authorization: Bearer ' +
+    c.setopt(c.HTTPHEADER, ['Authorization: Bearer ' + \
                             lifx_config.lifx_apikey])
     c.setopt(c.CUSTOMREQUEST, 'PUT')
     c.setopt(c.POSTFIELDS, data)
@@ -64,13 +64,13 @@ def handle_lifx_message(client, msg, smsg):
     if(len(parts) == 2):
         if(cmd == "turn_on"):  # !turn_on,light_id
             light_id = parts[1]
-            print(_lifx_json_put("/lights/id:" +
+            print(_lifx_json_put("/lights/id:" + \
                                  light_id + "/power",
                                  "state=on&duration=0.0"))
 
         if(cmd == "turn_off"):  # !turn_off,light_id
             light_id = parts[1]
-            print(_lifx_json_put("/lights/id:" +
+            print(_lifx_json_put("/lights/id:" + \
                                  light_id + "/power",
                                  "state=off&duration=0.0"))
 
@@ -78,7 +78,7 @@ def handle_lifx_message(client, msg, smsg):
         if(cmd == "set_brightness"):  # !turn_off,light_id
             light_id = parts[1]
             brightness = parts[2]
-            print(_lifx_json_put("/lights/id:" +
+            print(_lifx_json_put("/lights/id:" + \
                                  light_id + "/color",
-                                 "power_on=false&duration=0.0" +
+                                 "power_on=false&duration=0.0" + \
                                  "&color=brightness:" + brightness))
